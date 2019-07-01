d61381bed13f3d7b1c7b4df6fef979f2d4183da4
susgupta <guptask.7@gmail.com>
2019-07-01 11:59:27 -0400

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,22 @@
+# Sushil G JS Matrix Rain Using p5.js
+
+Sushil G JS Matrix Rain is an example that uses library from **p5.js**.  A link to **p5.js** is [here](https://p5js.org/).  The implementation relies on p5.js functionality to draw and 'animate' the effect.  
+
+The inspiration is from a tutorial from [Emily Xie Matrix Rain Tutorial](https://www.youtube.com/watch?v=S1TQCi9axzg)
+
+Intitially **Katakana** characters are used, like in the Matrix.
+Link To Unicode -> https://en.wikipedia.org/wiki/Katakana_(Unicode_block).
+
+## Technologies Used
+
+- HTML
+- Javascript
+- p5.js
+
+## Real-Time Viewing
+
+The application was publised using [GitHub Pages](https://pages.github.com/).
+
+Here is link to view the application:
+
+[Sushil G JS Matrix Rain Using p5.js](https://susgupta.github.io/js_p5_matrix_rain/index.html)
---
+++ js/main.js
@@ -0,0 +1,127 @@
+/**
+ * Using p5.js to make all things happen, extending functions from that
+ */
+
+//number of available characters
+const AVAILABLE_CHARACTERS = 96;
+//the starting unicde character
+const STARTING_UNICODE_CHARACTER = 0X30A0;
+
+var symbolSize = 24;
+var streams = [];
+
+//overridden from p5.js
+function setup() {
+
+    //carve out canvas area - the whole browser area
+    createCanvas(
+        window.innerWidth,
+        window.innerHeight
+    );
+    //make all black
+    background(0);
+
+    //get coords of stream
+    var x = 0;
+
+    //get number of streams based on screen size
+    for (var i = 0; i <= width / symbolSize; i++) {
+        //create Stream
+        var stream = new Stream();
+        var streamStartHeight = random(-1000, 0);
+        stream.generateSymbols(x, streamStartHeight);
+        //add to array
+        streams.push(stream);
+        //make stream beside
+        x += symbolSize;
+    }
+
+    textSize(symbolSize);
+}
+
+//overriden from p5.js
+function draw() {
+    //call p5.js to render background
+    background(0, 150);
+    //render all streams
+    streams.forEach(function (stream) {
+        stream.render();
+    });
+}
+
+//prototype for 'rain' symbol
+function Symbol(x, y, speed, first) {
+    this.x = x;
+    this.y = y;
+    this.value;
+    this.speed = speed;
+    this.switchInterval = round(random(2, 20));
+    this.first = first;
+
+    //create randomizer
+    this.setToRandomSymbol = function () {
+
+        //use p5 built in counter to determine when to generate symbol (reset at every end-frame)
+        if (frameCount % this.switchInterval === 0) {
+            //generate and set the symbol
+            this.value = String.fromCharCode(
+                STARTING_UNICODE_CHARACTER + round(random(0, AVAILABLE_CHARACTERS))
+            );
+        }
+    };
+
+    //make symbol rain down
+    this.rain = function () {
+
+        if (this.y >= height) {
+            //at bottom of canvas - reset height
+            this.y = 0
+        }
+        else {
+            //make it rain - with speed
+            this.y += this.speed;
+        }
+    };
+}
+
+//prototype for 'stream'
+function Stream() {
+    this.symbols = [];
+    this.totalSymbols = round(random(5, 30));
+    this.speed = random(5, 20);
+
+    //create symbols
+    this.generateSymbols = function (x, y) {
+        var first = round(random(0, 4)) == 1;
+
+        for (var i = 0; i <= this.totalSymbols; i++) {
+            //create a Symbol
+            var symbol = new Symbol(x, y, this.speed, first);
+            symbol.setToRandomSymbol();
+            //add to array
+            this.symbols.push(symbol);
+            //this will set next symbol above it
+            y -= symbolSize;
+            first = false;
+        }
+    }
+
+    //show symbols
+    this.render = function () {
+
+        this.symbols.forEach(function (symbol) {
+
+            //highlight if the symbol is first
+            if (symbol.first) {
+                fill(180, 255, 180);
+            }
+            else {
+                fill(0, 255, 70);
+            }
+
+            text(symbol.value, symbol.x, symbol.y);
+            symbol.rain();
+            symbol.setToRandomSymbol();
+        });
+    };
+}
